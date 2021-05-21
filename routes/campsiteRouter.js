fdb764f8e6c8a5059ee6a364331523e4ff16d905
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-05-21 18:16:24 -0400

    Express REST API with MongoDB and Mongoose Part 2

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -73,4 +73,146 @@ campsiteRouter.route('/:campsiteId')
     .catch(err => next(err));
 });
   
+campsiteRouter.route('/:campsiteId/comments')
+.get((req, res, next) => { 
+  Campsite.findById(req.params.campsiteId) //client looking for a single campsite's comments now using findById
+  .then(campsite => {
+    if (campsite) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(campsite.comments);
+    } else {
+      err = new Error(`Campsite ${req.params.campsiteId} not found`);
+      err.status = 404;
+      return next(err);
+    }
+  })   
+  .catch(err => next(err));
+})
+.post((req, res, next) => { 
+  Campsite.findById(req.params.campsiteId) 
+  .then(campsite => {
+    if (campsite) {
+      campsite.comments.push(req.body); //use array method to push new comment into comments arrray. Assuming request body has comment in it and express.json has already formatted it so we can push it in. Only changes comments array in app memory andn not subcomments doc in mongodb
+      campsite.save() //saves the new comment to mongodb database. Not a static method b/c method being performed on this instance the doc itself so little c for campsite
+      .then(campsite => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json(campsite);
+      })
+      .catch(err => next(err));
+    } else {
+      err = new Error(`Campsite ${req.params.campsiteId} not found`);
+      err.status = 404;
+      return next(err);
+    }
+  })   
+  .catch(err => next(err));
+})
+.put((req, res) => {
+  res.statusCode = 403;  
+  res.end(`PUT operation not supported on /campsties/${req.params.campsiteId}/comments`);
+})
+.delete((req, res, next) => {  
+  Campsite.findById(req.params.campsiteId)
+  .then(campsite => {
+    if (campsite) {
+      for (let i = (campsite.comments.length-1); i>=0; i--) {
+        //access each comment in comments sub doc array one at a time to remove them one by one
+        campsite.comments.id(campsite.comments[i]._id).remove();
+      } 
+      campsite.save()
+        .then(campsite => {
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(campsite);
+        })
+        .catch(err => next(err));
+    } else {
+      err = new Error(`Campsite ${req.params.campsiteId} not found`);
+      err.status = 404;
+      return next(err);
+    }
+  })
+    .catch(err => next(err));
+});
+
+campsiteRouter.route('/:campsiteId/comments/:commentId') //will handle requests of a specific comment id of a specific campsite id
+  .get((req, res, next) => {
+    Campsite.findById(req.params.campsiteId) 
+      .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) { //will retrieve the value of the comment subdoc with id passed in
+          res.statusCode = 200;
+          res.setHeader('Content-Type', 'application/json');
+          res.json(campsite.comments.id(req.params.commentId)); //safe to do b/c we have checked that there is a truthy value for comment
+        } else if (!campsite) { 
+          err = new Error(`Campsite ${req.params.campsiteId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+  })
+  .post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}/comments/${req.params.commentId}`);
+  })
+  .put((req, res, next) => { //we want to update existing data - don't want to update author, cannot update id or timestamp fields, so only want to update rating and comment text fields
+    Campsite.findById(req.params.campsiteId)
+      .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) { 
+          if (req.body.rating) {
+            campsite.comments.id(req.params.commentId).rating = req.body.rating;
+          }
+          if (req.body.text) {
+            campsite.comments.id(req.params.commentId).text = req.body.text;
+          } 
+          campsite.save()
+          .then(campsite => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(campsite);
+          })
+          .catch(err => next(err));
+        } else if (!campsite) {
+          err = new Error(`Campsite ${req.params.campsiteId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+  })
+  .delete((req, res, next) => {
+    Campsite.findById(req.params.campsiteId)
+      .then(campsite => {
+        if (campsite && campsite.comments.id(req.params.commentId)) {
+          campsite.comments.id(req.params.commentId).remove();
+          campsite.save()
+            .then(campsite => {
+              res.statusCode = 200;
+              res.setHeader('Content-Type', 'application/json');
+              res.json(campsite);
+            })
+            .catch(err => next(err));
+        } else if (!campsite) {
+          err = new Error(`Campsite ${req.params.campsiteId} not found`);
+          err.status = 404;
+          return next(err);
+        } else {
+          err = new Error(`Comment ${req.params.commentId} not found`);
+          err.status = 404;
+          return next(err);
+        }
+      })
+      .catch(err => next(err));
+  });
+
 module.exports = campsiteRouter;
