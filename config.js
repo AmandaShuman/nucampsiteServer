299e7127f246a7db08af682f93b98fceba97906a
Amanda Shuman <amanda.n.shuman@gmail.com>
2021-06-04 21:30:02 -0400

    Test

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
-module.exports = { 
-  'secretKey': '12345-67890-09876-54321', //what we will use to sign the json web token later
-  'mongoUrl': 'mongodb://localhost:27017/nucampsite' 
+module.exports = {
+  'secretKey': '12345-67890-09876-54321',
+  'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+  'facebook': {
+    clientId: '181744703863562',
+    clientSecret: 'YOUR FACEBOOK APP SECRET HERE'
+  }
 }
